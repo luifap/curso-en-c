fdb7b2914d1f73f76829cd286c27593ec43ea982
luifap <52289372+luifap@users.noreply.github.com>
2020-06-16 22:29:25 -0500

    condicionales if - else

---
+++ condicionales.c
@@ -0,0 +1,19 @@
+#include <stdio.h> 
+int main() 
+{
+  int edad;
+  printf("ingresa tu edad\n");
+  scanf("%d",&edad);
+  if(edad >=18)
+  {
+    printf("eres mayor de edad\n");
+  } 
+  else if(edad == 17)
+  {
+    printf("Casi eres mayor de edad\n");
+  }
+  else{
+    printf("eres menor de edad\n");
+  }
+  return 0;
+}
