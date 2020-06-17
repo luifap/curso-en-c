d034cf1824482a974b814135fea79504b9f8c1c2
luifap <52289372+luifap@users.noreply.github.com>
2020-06-16 22:24:03 -0500

    Ciclo for

---
+++ cicloforone.c
@@ -0,0 +1,13 @@
+/*
+Ciclo For en C
+*/
+#include <stdio.h>
+int main()
+{
+int contador;
+for(contador = 0;contador <=10; contador++)
+  	{
+      printf("%i\n", contador);
+    }
+    return 0;
+}
