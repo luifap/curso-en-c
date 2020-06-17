f6387a464fb4961711781fcd5a1acc1204aedf98
luifap <52289372+luifap@users.noreply.github.com>
2020-06-16 22:15:30 -0500

    Arreglos unidimensionales

---
+++ arrayunidime.c
@@ -0,0 +1,10 @@
+/*
+Arreglos en C (unidimensionales)
+*/
+#include <stdio.h>
+int main()
+{
+  int arreglo[4] = {3,4,1,5} ; //colocamos nuestro nombre de arreglo y entre corchetos la dimensión y definimos los valores del arreglo los cuales van entre llaves y separados por coma, el valor para este caso es 4 van del 0 - al 3
+  printf("%i\n",arreglo[2]); //imprimimos nuestro arreglo en este caso debe imprimir el 1
+  return 0;//indica que el programa termino
+}//fin de la posición main
